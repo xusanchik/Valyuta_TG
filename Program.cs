767ace34403f2e6c229ad1e53cb73bf52e4d3935
xusanchik <samadovxusan9013@gmail.com>
2024-01-17 08:13:26 +0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Text.Json;
+
+
+namespace Valyuta_TG
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+
+            const string link = ("6444103575:AAFHFegeFM8Bw53IGRzMOZYliio4hRd9Fxs");
+            System_valyuta system_Valyuta = new System_valyuta(link);
+            await system_Valyuta.BotHandle();
+
+            try
+            {
+                await system_Valyuta.BotHandle();
+
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Xatoku!");
+            }
+        }
+    }
+}
